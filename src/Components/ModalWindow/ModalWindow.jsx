b3901330cdc25989445792945290977efc515b9c
xkochevnikx <xkochevnik@protonmail.com>
2023-05-29 17:40:38 +0300

    fix

--- src/Components/ModalWindow/ModalWindow.jsx
+++ src/Components/ModalWindow/ModalWindow.jsx
@@ -1,28 +1,27 @@
-import React from "react";
-import "./ModalWindow.css";
+import React from 'react';
+import './ModalWindow.css';
 
 //? в пропсах можно передать children а так же принять их ниже в контентной части. И в родительском компоненте обернуть этих детей в компонент.
 const ModalWindow = ({ activeModal, setActiveModal, items }) => {
-  console.log(activeModal);
-  return (
-    <>
-      <div
-        className={activeModal ? "modal_active" : "modal"}
-        onClick={() => setActiveModal(false)}>
-        <div
-          className={activeModal ? "modal_content_active" : "modal_content"}
-          onClick={e => e.stopPropagation()}>
-          <ul>
-            {items.map(item => (
-              <li>
-                {item.value} - {item.href}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div
+                className={activeModal ? 'modal_active' : 'modal'}
+                onClick={() => setActiveModal(false)}>
+                <div
+                    className={
+                        activeModal ? 'modal_content_active' : 'modal_content'
+                    }
+                    onClick={(e) => e.stopPropagation()}>
+                    <ul>
+                        {items.map((item) => (
+                            <a href={item.href}>{item.value}</a>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ModalWindow;
